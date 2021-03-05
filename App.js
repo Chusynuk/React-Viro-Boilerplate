aa891d54836a392dce901546703a1ee23445f553
eloi <recycled_loops@hotmail.com>
2021-03-05 18:09:37 +0100

    add eslint and prettier

--- App.js
+++ App.js
@@ -1,142 +1,128 @@
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  Text,
-  View,
-  StyleSheet,
-  PixelRatio,
-  TouchableHighlight,
-} from 'react-native';
+import { AppRegistry, Text, View, StyleSheet, PixelRatio, TouchableHighlight } from 'react-native';
 
-import {
-  ViroVRSceneNavigator,
-  ViroARSceneNavigator
-} from 'react-viro';
+import { ViroVRSceneNavigator, ViroARSceneNavigator } from 'react-viro';
 
 import InitialARScene from './js/HelloWorldSceneAR';
 /*
  TODO: Insert your API key below
  */
 var sharedProps = {
-  apiKey:"API_KEY_HERE",
-}
+    apiKey: 'API_KEY_HERE'
+};
 
 // Sets the default scene you want for AR and VR
 // var InitialARScene = require('./js/HelloWorldSceneAR');
 var InitialVRScene = require('./js/HelloWorldScene');
 
-var UNSET = "UNSET";
-var VR_NAVIGATOR_TYPE = "VR";
-var AR_NAVIGATOR_TYPE = "AR";
+var UNSET = 'UNSET';
+var VR_NAVIGATOR_TYPE = 'VR';
+var AR_NAVIGATOR_TYPE = 'AR';
 
 // This determines which type of experience to launch in, or UNSET, if the user should
 // be presented with a choice of AR or VR. By default, we offer the user a choice.
 var defaultNavigatorType = UNSET;
 
-
 const ViroSample = () => {
-  const [navigatorType, setNavigatorType] = React.useState('UNSET');
-  const [sharedProps, setSharedProps] = React.useState();
+    const [navigatorType, setNavigatorType] = React.useState('UNSET');
+    const [sharedProps, setSharedProps] = React.useState();
 
-  // Returns the ViroARSceneNavigator which will start the AR experience
-  const getARNavigator = () => {
-    return (
-      <ViroARSceneNavigator {...sharedProps}
-        initialScene={{scene: InitialARScene}} />
-    );
-  }
+    // Returns the ViroARSceneNavigator which will start the AR experience
+    const getARNavigator = () => {
+        return <ViroARSceneNavigator {...sharedProps} initialScene={{ scene: InitialARScene }} />;
+    };
 
-   // This function returns an anonymous/lambda function to be used
-  // by the experience selector buttons
-  const getExperienceButtonOnPress = (navigatorType) => {
-    setNavigatorType(navigatorType)
-  }
+    // This function returns an anonymous/lambda function to be used
+    // by the experience selector buttons
+    const getExperienceButtonOnPress = (navigatorType) => {
+        setNavigatorType(navigatorType);
+    };
 
-  // This function "exits" Viro by setting the navigatorType to UNSET.
-  const exitViro = () => {
-    setNavigatorType('UNSET')
-  }
+    // This function "exits" Viro by setting the navigatorType to UNSET.
+    const exitViro = () => {
+        setNavigatorType('UNSET');
+    };
 
-  if (navigatorType == UNSET) {
-    return (
-      <View style={localStyles.outer} >
-        <View style={localStyles.inner} >
-          <Text style={localStyles.titleText}>
-            Choose your desired experience:
-          </Text>
-          <TouchableHighlight style={localStyles.buttons}
-            onPress={() => getExperienceButtonOnPress(AR_NAVIGATOR_TYPE)}
-            underlayColor={'#68a0ff'} >
-            <Text style={localStyles.buttonText}>AR</Text>
-          </TouchableHighlight>
-          <TouchableHighlight style={localStyles.buttons}
-            onPress={() => getExperienceButtonOnPress(VR_NAVIGATOR_TYPE)}
-            underlayColor={'#68a0ff'} >
-            <Text style={localStyles.buttonText}>VR</Text>
-          </TouchableHighlight>
-        </View>
-      </View>
-    );
-  } else if (navigatorType == VR_NAVIGATOR_TYPE) {
-    return getVRNavigator();
-  } else if (navigatorType == AR_NAVIGATOR_TYPE) {
-    return getARNavigator();
-  }
+    if (navigatorType == UNSET) {
+        return (
+            <View style={localStyles.outer}>
+                <View style={localStyles.inner}>
+                    <Text style={localStyles.titleText}>Choose your desired experience:</Text>
+                    <TouchableHighlight
+                        style={localStyles.buttons}
+                        onPress={() => getExperienceButtonOnPress(AR_NAVIGATOR_TYPE)}
+                        underlayColor={'#68a0ff'}>
+                        <Text style={localStyles.buttonText}>AR</Text>
+                    </TouchableHighlight>
+                    <TouchableHighlight
+                        style={localStyles.buttons}
+                        onPress={() => getExperienceButtonOnPress(VR_NAVIGATOR_TYPE)}
+                        underlayColor={'#68a0ff'}>
+                        <Text style={localStyles.buttonText}>VR</Text>
+                    </TouchableHighlight>
+                </View>
+            </View>
+        );
+    } else if (navigatorType == VR_NAVIGATOR_TYPE) {
+        return getVRNavigator();
+    } else if (navigatorType == AR_NAVIGATOR_TYPE) {
+        return getARNavigator();
+    }
 };
 
 var localStyles = StyleSheet.create({
-  viroContainer :{
-    flex : 1,
-    backgroundColor: "black",
-  },
-  outer : {
-    flex : 1,
-    flexDirection: 'row',
-    alignItems:'center',
-    backgroundColor: "black",
-  },
-  inner: {
-    flex : 1,
-    flexDirection: 'column',
-    alignItems:'center',
-    backgroundColor: "black",
-  },
-  titleText: {
-    paddingTop: 30,
-    paddingBottom: 20,
-    color:'#fff',
-    textAlign:'center',
-    fontSize : 25
-  },
-  buttonText: {
-    color:'#fff',
-    textAlign:'center',
-    fontSize : 20
-  },
-  buttons : {
-    height: 80,
-    width: 150,
-    paddingTop:20,
-    paddingBottom:20,
-    marginTop: 10,
-    marginBottom: 10,
-    backgroundColor:'#68a0cf',
-    borderRadius: 10,
-    borderWidth: 1,
-    borderColor: '#fff',
-  },
-  exitButton : {
-    height: 50,
-    width: 100,
-    paddingTop:10,
-    paddingBottom:10,
-    marginTop: 10,
-    marginBottom: 10,
-    backgroundColor:'#68a0cf',
-    borderRadius: 10,
-    borderWidth: 1,
-    borderColor: '#fff',
-  }
+    viroContainer: {
+        flex: 1,
+        backgroundColor: 'black'
+    },
+    outer: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: 'black'
+    },
+    inner: {
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'center',
+        backgroundColor: 'black'
+    },
+    titleText: {
+        paddingTop: 30,
+        paddingBottom: 20,
+        color: '#fff',
+        textAlign: 'center',
+        fontSize: 25
+    },
+    buttonText: {
+        color: '#fff',
+        textAlign: 'center',
+        fontSize: 20
+    },
+    buttons: {
+        height: 80,
+        width: 150,
+        paddingTop: 20,
+        paddingBottom: 20,
+        marginTop: 10,
+        marginBottom: 10,
+        backgroundColor: '#68a0cf',
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: '#fff'
+    },
+    exitButton: {
+        height: 50,
+        width: 100,
+        paddingTop: 10,
+        paddingBottom: 10,
+        marginTop: 10,
+        marginBottom: 10,
+        backgroundColor: '#68a0cf',
+        borderRadius: 10,
+        borderWidth: 1,
+        borderColor: '#fff'
+    }
 });
 
-export default ViroSample
+export default ViroSample;
--- js/HelloWorldSceneAR.js
+++ js/HelloWorldSceneAR.js
@@ -1,41 +1,41 @@
 'use strict';
 
 import React, { Component } from 'react';
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from 'react-native';
 
-import {
-  ViroARScene,
-  ViroText,
-  ViroConstants,
-  ViroBox
-} from 'react-viro';
+import { ViroARScene, ViroText, ViroConstants, ViroBox } from 'react-viro';
 
 const HelloWorldSceneAR = () => {
-    const [text, setTest] = React.useState("Initializing AR...");
+    const [text, setTest] = React.useState('Initializing AR...');
 
     const onInitialized = (state, reason) => {
-      if (state == ViroConstants.TRACKING_NORMAL) {
-        setTest("Hello World!");
-      } else if (state == ViroConstants.TRACKING_NONE) {
-        // Handle loss of tracking
-      }
-    }
-      return (
-        <ViroARScene onTrackingUpdated={onInitialized} >
-          <ViroBox position={[0, -.5, -1]} scale={[.3, .3, .1]} />
-          <ViroText text={text} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} />
+        if (state == ViroConstants.TRACKING_NORMAL) {
+            setTest('Hello World!');
+        } else if (state == ViroConstants.TRACKING_NONE) {
+            // Handle loss of tracking
+        }
+    };
+    return (
+        <ViroARScene onTrackingUpdated={onInitialized}>
+            <ViroBox position={[0, -0.5, -1]} scale={[0.3, 0.3, 0.1]} />
+            <ViroText
+                text={text}
+                scale={[0.5, 0.5, 0.5]}
+                position={[0, 0, -1]}
+                style={styles.helloWorldTextStyle}
+            />
         </ViroARScene>
-  );
-}
+    );
+};
 
 var styles = StyleSheet.create({
-  helloWorldTextStyle: {
-    fontFamily: 'Arial',
-    fontSize: 30,
-    color: '#ffffff',
-    textAlignVertical: 'center',
-    textAlign: 'center',  
-  },
+    helloWorldTextStyle: {
+        fontFamily: 'Arial',
+        fontSize: 30,
+        color: '#ffffff',
+        textAlignVertical: 'center',
+        textAlign: 'center'
+    }
 });
 
 export default HelloWorldSceneAR;
