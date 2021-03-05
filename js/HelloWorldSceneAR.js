2f79b981ad2cb7a4dbf1635f59305c35cec2f70c
eloi <recycled_loops@hotmail.com>
2021-03-05 15:57:50 +0100

    cleanup

--- js/HelloWorldSceneAR.js
+++ js/HelloWorldSceneAR.js
@@ -1,7 +1,6 @@
 'use strict';
 
 import React, { Component } from 'react';
-
 import {StyleSheet} from 'react-native';
 
 import {
@@ -21,13 +20,12 @@ const HelloWorldSceneAR = () => {
         // Handle loss of tracking
       }
     }
-  return (
-    <ViroARScene onTrackingUpdated={onInitialized} >
-      <ViroBox position={[0, -.5, -1]} scale={[.3, .3, .1]} />
-      <ViroText text={text} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} />
-    </ViroARScene>
+      return (
+        <ViroARScene onTrackingUpdated={onInitialized} >
+          <ViroBox position={[0, -.5, -1]} scale={[.3, .3, .1]} />
+          <ViroText text={text} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} />
+        </ViroARScene>
   );
-
 }
 
 var styles = StyleSheet.create({
@@ -41,42 +39,3 @@ var styles = StyleSheet.create({
 });
 
 export default HelloWorldSceneAR;
-
-// export default class HelloWorldSceneAR extends Component {
-
-//   constructor() {
-//     super();
-
-//     // Set initial state here
-//     this.state = {
-//       text : "Initializing AR..."
-//     };
-
-//     // bind 'this' to functions
-//     this._onInitialized = this._onInitialized.bind(this);
-//   }
-
-//   render() {
-//     return (
-//       <ViroARScene onTrackingUpdated={this._onInitialized} >
-//         <ViroBox position={[0, -.5, -1]} scale={[.3, .3, .1]}
-//          />
-//         <ViroText text={this.state.text} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} />
-//       </ViroARScene>
-//     );
-//   }
-
-//   _onInitialized(state, reason) {
-//     if (state == ViroConstants.TRACKING_NORMAL) {
-//       this.setState({
-//         text : "Hello World!"
-//       });
-//     } else if (state == ViroConstants.TRACKING_NONE) {
-//       // Handle loss of tracking
-//     }
-//   }
-// }
-
-
-
-// module.exports = HelloWorldSceneAR;
